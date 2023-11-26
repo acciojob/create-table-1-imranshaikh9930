58baf945949f8183d1c5d7ac7827e9b695a98a2f
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-26 15:30:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+let rows = 2;
+
+
 function insert_Row() {
-    //Write your code here
-  
-  
+    let cells = 0;
+  const table = document.querySelector("#sampleTable");
+  const tr = document.createElement("tr");
+  const td1 = document.createElement("td");
+  const td2 = document.createElement("td");
+
+  rows++;
+
+
+  td1.innerText = `Row${rows} cell${cells+1}`;
+
+  td2.innerText = `Row${rows} cell${cells+2}`;
+
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  table.appendChild(tr);
 }
